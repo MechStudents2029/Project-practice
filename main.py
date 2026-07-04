8969b90eacbd18181092a361e226132692dfa4c7
MechStudents2029 <arouzaqui3@gmail.com>
2026-07-04 01:01:15 -0400

    Added a task input with exit condition

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print ("Task Manager is Running....")
+tasks = ["Homework", "Pooping", "Cleaning", "Ironing"]
+
+while True:
+    new_tasks = input("Enter a task (or type 'exit' to stop): ")
+
+    if new_tasks == "exit":
+        break
+    tasks.append(new_tasks)
+
+print(tasks)
